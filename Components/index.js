c8616d9ba685563fb5b3418aea124e649f5affba
ABDELHAK MEHDA <abdelhakmehda@gmail.com>
2023-03-10 22:56:47 +0100

    add NFT card

---
+++ Components/index.js
@@ -0,0 +1,14 @@
+import { CircleButton, RectButton } from "./Button";
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NftCard from "./NftCard";
+import SubInfo, { NFTTitle } from "./SubInfo";
+export {
+    FocusedStatusBar,
+    HomeHeader,
+    NftCard,
+    CircleButton,
+    RectButton,
+    NFTTitle,
+
+}
